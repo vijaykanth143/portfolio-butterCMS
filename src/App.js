b39014716151a752d074bd0cf1e4c162f5369c5f
vijaykanth <87868293+vijaykanth143@users.noreply.github.com>
2023-07-19 15:42:00 +0530

    hii

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import "./App.css";
+import About from "./Components/About";
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
+import Skills from "./Components/Skills";
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
     </div>
   );
 }
